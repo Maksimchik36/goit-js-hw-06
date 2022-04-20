89950c7dd566f149cd14bdd61f9868fd1e256179
Maksimchik36 <Peltek1985@gmail.com>
2022-04-20 22:20:48 +0300

    add all tasks

---
+++ js/task-01.js
@@ -0,0 +1,25 @@
+// 1. Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
+
+
+const categoriesEl = document.querySelectorAll("#categories li.item");
+
+const countCategories = (category) => {
+    console.log(`Number of categories: ${category.length}`)  
+}
+
+countCategories(categoriesEl);
+
+// 2. Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
+// В результате, в консоли будут выведены такие сообщения.
+
+// Number of categories: 3
+
+// Category: Animals
+// Elements: 4
+
+// Category: Products
+// Elements: 3
+
+// Category: Technologies
+// Elements: 5
+
