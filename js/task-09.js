bddd531d4be31156f01c4290a23fe0989aafcd7b
Maksimchik36 <Peltek1985@gmail.com>
2022-04-26 13:57:33 +0300

    task-9 complet

--- js/task-09.js
+++ js/task-09.js
@@ -2,12 +2,28 @@
 // Для генерации случайного цвета используй функцию getRandomHexColor.
 
 function getRandomHexColor() {
-  return Math.floor(Math.random() * 16777215).toString(16);
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 const bodyRef = document.querySelector("body");
 
 const buttonRef = document.querySelector(".change-color");
 
+const colorBodyRef = document.querySelector(".color");
 
-// buttonRef.addEventListener("click", callback)
+buttonRef.addEventListener("click", changeColor);
+
+function changeColor(event) {
+  if (event.target.nodeName !== "BUTTON") {
+    return;
+  }
+  
+  colorBodyRef.textContent = `- ${getRandomHexColor()}`;
+  bodyRef.style.backgroundColor = `${getRandomHexColor()}`;
+
+}
+
+
+
